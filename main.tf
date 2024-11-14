23d5b16793fe565ef8121a102ce94ff617cabc2f
Sin11021219 <sinya11021219@gmail.com>
2024-11-15 00:01:24 +0900

    main

--- main.tf
+++ main.tf
@@ -44,6 +44,47 @@ terraform {
   }
 }
 
+required_providers {
+  aws = {
+    source = "hashicorp/aws"
+    version = "~> 4.0"
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
 
 //GiHubでコミット
 $ git add. && git commit -m "Backend Configuration"
+
+
+//復習3回目
+//Terraformでバックエンドの設定
+$ mkdir aws-ecs-terraform && cd aws-ecs-terraform
+$ touch main.tf
+
+terraform {
+   backend "s3" {
+     bucket         = "aws-ecs-terraform-tfstate"
+     key            = "terraform.tfstate"
+     region         = "ap-northeast-1"
+     dynamodb_table = "aws-ecs-terraform-tfstate-locking"
+     encrypt        =  true
+   }
+}
+
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 4.0"
+  }
+}
+
+provider "aws" {
+ region = ap-northeast-1
+}
+
+//GitHubでコミット
+$ git add . $$ git commit -m "Backend Configuration"
+
